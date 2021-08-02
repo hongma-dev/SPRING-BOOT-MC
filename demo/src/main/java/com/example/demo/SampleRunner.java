d5fe521d45be369ecad49c5b4883b07a589054cb
hongma-dev <lovely4750@gmail.com>
2021-08-02 19:41:55 +0900

    로깅관련 학습

--- demo/src/main/java/com/example/demo/SampleRunner.java
+++ demo/src/main/java/com/example/demo/SampleRunner.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class SampleRunner implements ApplicationRunner{
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	HongmaProperties prop;
@@ -17,10 +21,14 @@ public class SampleRunner implements ApplicationRunner{
 	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		System.out.println("============================");
-		System.out.println("name : "+prop.getName());
-		System.out.println("hello : "+hello);
-		System.out.println("============================");
+		logger.debug("============================");
+		logger.debug("test");
+		logger.debug("============================");
+		
+//		System.out.println("============================");
+//		System.out.println("name : "+prop.getName());
+//		System.out.println("hello : "+hello);
+//		System.out.println("============================");
 	}
 
 }
