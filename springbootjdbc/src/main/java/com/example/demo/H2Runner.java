5acc4d1bfd198efd44ef0b6285f393f5dca5af6b
hongma-dev <lovely4750@gmail.com>
2021-08-09 19:44:15 +0900

    스프링DATA 인메모리 데이터베이스 학습

---
+++ springbootjdbc/src/main/java/com/example/demo/H2Runner.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class H2Runner implements ApplicationRunner{
+
+	
+	@Autowired
+	DataSource dataSource;
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		try (Connection connection = dataSource.getConnection()){
+			
+			System.out.println(connection.getMetaData().getURL());
+			System.out.println(connection.getMetaData().getUserName());
+			
+			
+			Statement statement = connection.createStatement();
+			String sql = "CREATE TABLE USER( ID INTEGER NOT NULL, NAME VARCHAR(255) , PRIMARY KEY(ID) ) ";
+			statement.executeUpdate(sql);
+			
+			connection.close();
+			
+			
+			jdbcTemplate.execute("INSERT INTO USER VALUES (1, 'HONGMA') " );
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+
+	
+}
